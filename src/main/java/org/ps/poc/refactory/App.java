91861d9274cf6b4f3ffe6ea0fe91c5553ca5941e
Paulo R. A. Sales <salespaulo08@gmail.com>
2018-04-09 14:35:27 -0300

    Retirado o banner do GV

--- src/main/java/org/ps/poc/refactory/App.java
+++ src/main/java/org/ps/poc/refactory/App.java
@@ -11,7 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 public class App {
 
 	public static void main(String[] args) {
-		healthy(new SpringApplicationBuilder(App.class).banner(new GVBanner()).run(args));
+		healthy(new SpringApplicationBuilder(App.class).run());
 	}
 
 	private static void healthy(ConfigurableApplicationContext context) {
