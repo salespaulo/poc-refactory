924cabc05e5c606263a194fc3146ccaa3df2158b
Paulo R. A. Sales <salespaulo08@gmail.com>
2018-04-09 14:40:25 -0300

    Melhorias no codigo...

--- src/main/java/org/ps/poc/refactory/ticket/TicketBookingServiceImpl.java
+++ src/main/java/org/ps/poc/refactory/ticket/TicketBookingServiceImpl.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import javax.transaction.Transactional;
 
